5da803f596628f9807a89924d94996bc669f7b12
S.Panfilov <se-panfilov@ya.ru>
2015-07-31 17:21:47 +0800

    Simplify to one-page

--- index.js
+++ index.js
@@ -3,11 +3,7 @@
 angular.module('sp-io.index.page', [])
 
     .controller('IndexPageCtrl', function ($scope) {
-
-        $scope.skills = [
-            {title: "JS", name: "JavaScript", bgClass: 'red'},
-            {title: "ng", name: "Angularjs", bgClass: 'blue'}
-        ];
+        //
     })
 
 
