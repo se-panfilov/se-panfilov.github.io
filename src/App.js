1f787bb6714b3672c3e479823a29ff0630e15265
Sergei Panfilov <se-panfilov@ya.ru>
2019-07-05 15:44:11 +0200

    added projects page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Landing} from './landing/Landing'
+import {Projects} from './projects/Projects'
 
 function App() {
   return (
     <div className="content">
       <Landing/>
+      <Projects/>
     </div>
   )
 }
---
+++ src/projects/Projects.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export class Projects extends Component {
+  render() {
+    return (
+      <div className="projects">
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    )
+  }
+}
