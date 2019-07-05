4055a2037024799524f6974d03c161463a800be0
Sergei Panfilov <se-panfilov@ya.ru>
2019-07-05 10:34:06 +0200

    enabled hmr

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const render = () => ReactDOM.render(<App/>, document.getElementById('root'));
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
